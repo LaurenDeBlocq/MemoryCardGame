438127230a8666617ec8b0c5b31beb028a5b4de8
Lauren de Blocq van Scheltinga <lauren.deblocq@gmail.com>
2024-03-08 14:54:07 +0200

    Created the components required for the header and added basic styling

--- memory-card-game/src/components/Header.jsx
+++ memory-card-game/src/components/Header.jsx
@@ -1,5 +1,15 @@
 const Header = () => {
-  return <div>This is the header</div>;
+  return (
+    <header className="header">
+      <h2 className="header--text">Memory</h2>
+      <button className="header--button header--button-restart poppins-bold">
+        Restart Game
+      </button>
+      <button className="header--button header--button-exit poppins-bold">
+        Exit Game
+      </button>
+    </header>
+  );
 };
 
 export default Header;
--- memory-card-game/src/index.jsx
+++ memory-card-game/src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
