73ec9fc7d8a4afd8a4a15df57ec7e8138a0f0473
Lauren de Blocq van Scheltinga <lauren.deblocq@gmail.com>
2024-03-08 15:55:53 +0200

    Finished styling Header

--- memory-card-game/src/components/Header.jsx
+++ memory-card-game/src/components/Header.jsx
@@ -1,13 +1,16 @@
 const Header = () => {
   return (
     <header className="header">
+      <div className="header--grid"></div>
       <h2 className="header--text">Memory</h2>
-      <button className="header--button header--button-restart poppins-bold">
-        Restart Game
-      </button>
-      <button className="header--button header--button-exit poppins-bold">
-        Exit Game
-      </button>
+      <div className="header--button-section">
+        <button className="header--button header--button-restart poppins-bold">
+          Restart Game
+        </button>
+        <button className="header--button header--button-exit poppins-bold">
+          Exit Game
+        </button>
+      </div>
     </header>
   );
 };
