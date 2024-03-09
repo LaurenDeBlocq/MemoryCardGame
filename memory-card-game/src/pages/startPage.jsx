be3742b0333d2961b9c9fe8975aa5137d3016e1f
Lauren de Blocq van Scheltinga <lauren.deblocq@gmail.com>
2024-03-09 10:27:28 +0200

    Created required elements for the starting page.

--- memory-card-game/src/pages/startPage.jsx
+++ memory-card-game/src/pages/startPage.jsx
@@ -1,9 +1,29 @@
+import playerOne from "../resources/images/player1.png";
+import playerTwo from "../resources/images/player2.png";
+
 const StartPage = () => {
   return (
     <>
-      <div>
-        <h1>Hello World!</h1>
+      <h1 className="start-page--text">Are you ready to play?</h1>
+      <div className="start-page--player-container">
+        <div className="start-page--player-one">
+          <img src={playerOne} alt="player 1 image" />
+          <input
+            id="player-one-name"
+            type="text"
+            placeholder="name of player 1"
+          />
+        </div>
+        <div className="start-page--player-two">
+          <img src={playerTwo} alt="player 2 image" />
+          <input
+            id="player-two-name"
+            type="text"
+            placeholder="name of player 2"
+          />
+        </div>
       </div>
+      <button className="start-page--start-button">Let's play!</button>
     </>
   );
 };
