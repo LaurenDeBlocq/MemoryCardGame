4e5b3294d173162b70b23e22901b0dca970a475a
Lauren de Blocq van Scheltinga <lauren.deblocq@gmail.com>
2024-03-09 11:58:44 +0200

    Removed unnecessary scoreSlice related code.

--- memory-card-game/src/app/store.js
+++ memory-card-game/src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import scoreReducer from "../slices/scoreSlice";
 import playerOneReducer from "../slices/playerOneSlice";
 import playerTwoReducer from "../slices/playerTwoSlice";
 
 export default configureStore({
   reducer: {
-    score: scoreReducer,
     playerOne: playerOneReducer,
     playerTwo: playerTwoReducer,
   },
