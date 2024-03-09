a9205c785bdd98b7ec12531659bc7db00cd94730
Lauren de Blocq van Scheltinga <lauren.deblocq@gmail.com>
2024-03-09 20:51:46 +0200

    Randomised the card on the board and removed errant console.log()

--- memory-card-game/src/components/GameBoard.jsx
+++ memory-card-game/src/components/GameBoard.jsx
@@ -2,6 +2,13 @@
 import Card from "./Card";
 
 const GameBoard = ({ cardList }) => {
+  for (let i = cardList.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let k = cardList[i];
+    cardList[i] = cardList[j];
+    cardList[j] = k;
+  }
+
   const cardsToLoad = cardList.map((card) => {
     return <Card key={card.index} data={card} />;
   });
